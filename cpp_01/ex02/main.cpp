917f3545f3482c4d682fb9911c573023a23ab3d1
jazzazi007 <maaljazzazi22@eng.just.edu.jo>
2025-11-23 21:58:27 +0300

    cpp ex02

---
+++ cpp_01/ex02/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+/*
+Write a program that contains:
+• A string variable initialized to "HI THIS IS BRAIN".
+• stringPTR: a pointer to the string.
+• stringREF: a reference to the string.
+Your program must print:
+• The memory address of the string variable.
+• The memory address held by stringPTR.
+• The memory address held by stringREF.
+And then:
+• The value of the string variable.
+• The value pointed to by stringPTR.
+• The value pointed to by stringREF.
+That’s all—no tricks. The goal of this exercise is to demystify references, which may
+seem completely new. Although there are some small differences, this is simply another
+syntax for something you already do: address manipulation.*/
+
+int main()
+{
+    std::string str = "HI THIS IS BRAIN";
+    std::string *stringPTR = &str;
+    std::string &stringREF = str;
+    std::cout << "Address of string variable: " << &str << std::endl;
+    std::cout << "Address held by stringPTR: " << stringPTR << std::endl;
+    std::cout << "Address held by stringREF: " << &stringREF << std::endl;
+    std::cout << "Value of string variable: " << str << std::endl;
+    std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+    return 0;
+}
